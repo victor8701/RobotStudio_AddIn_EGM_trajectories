6c774e9199a9c7bbdaf8e2de529df60bca313dce
Victor Martin Parra <100428714@alumnos.uc3m.es>
2025-05-20 16:21:32 +0200

    Delete of Load Controller Button
    - SimpleButton3 deleted.
    - SimpleButton2 -> loads controller (only the first time it is pressed), then it loads .csv file and creates paths and targets from it.
    - Correct calls in Class1.cs

--- TFG_offline/TFG_offline/Buttons/SimpleButton2.cs
+++ TFG_offline/TFG_offline/Buttons/SimpleButton2.cs
@@ -1,18 +1,21 @@
-﻿using TFG_offline.Controller;
+using TFG_offline.Buttons;
 using TFG_offline.Paths;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TFG_offline.Files;
+using TFG_offline.Controller;
+using ABB.Robotics.RobotStudio;
 
-namespace  TFG_offline.Buttons
+namespace TFG_offline.Buttons
 {
-    internal class SimpleButton2
+    internal class SimpleButton3
     {
         public static void Pressed()
         {
-            LoadController.Load();
+                Path_list.GetStationPaths();
         }
     }
 }
